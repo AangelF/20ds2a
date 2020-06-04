d8b026cc80c192f01f70df858ee3b617f341b33a
Angel <60322727+AangelF@users.noreply.github.com>
2020-06-04 11:39:12 -0500

    Add files via upload

---
+++ 1p/p2/emptyArray.js
@@ -0,0 +1,10 @@
+var arrray1 = []
+let array2 = new Array()
+array3 = []
+   
+let avgTempJan=31.9   
+let avgTempMay=34.3
+let avgTempMar=34.3
+
+let anArray = [-3,-2,1]
+console.log(anArray)
---
+++ 1p/p2/taskNecho.js
@@ -0,0 +1,9 @@
+function task(){
+    console.log('performig a task...')
+}
+function echo (p){
+    return p
+}
+console.log(echo(-3))
+task()
+console.log('done..')
