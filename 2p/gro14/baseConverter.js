7fbc82586203e984d19be3ba5235f51a7423f5b0
Angel <60322727+AangelF@users.noreply.github.com>
2020-06-15 03:06:30 -0500

    Update baseConverter.js

--- 2p/gro14/baseConverter.js
+++ 2p/gro14/baseConverter.js
@@ -1,3 +1,27 @@
+  function Stack() {
+    let items = []
+    this.push = function(element) {
+        items.push(element)
+    }
+    this.pop = function() {
+        return items.pop()
+    }
+    this.peek = function() {
+        return items[items.length - 1]
+    }
+    this.isEmpty = function() {
+        return items.length == 0
+    }
+    this.size = function() {
+        return items.length
+    }
+    this.clear = function() {
+        items = []
+    }
+    this.print = function() {
+        console.log(items.toString())
+    }
+}
 function baseConverter(decNumber, base){
  var remStack = new Stack(),
  rem,
